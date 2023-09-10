3b67ac3e8fd289884c66fcbf59550b9f05e40c5e
Jose <jose.dev2708@gmail.com>
2023-09-09 20:45:20 -0400

    chore: create server bootstrap

--- router/user.router.go
+++ router/user.router.go
@@ -1,19 +1,36 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/bricsport/common/exceptions"
+	"github.com/bricsport/handler"
+	"github.com/gin-gonic/gin"
+)
 
 var prefix = "/api/v2"
 
-func LoadUserRoutes(router *gin.Engine) {
+func LoadUserRoutes(router *gin.Engine, controller handler.UserHandler) {
 
-	// gin.HandlerFunc
 	authRouter := router.Group(prefix)
 
-	authRouter.GET("/ping", func(c *gin.Context) {
-		c.String(200, "auth pong")
-	})
+	// router.GET("/")
+
+	authRouter.POST("/", makeGinFunc(controller.Create))
 
 	router.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
+}
+
+// TODO: move this type later
+type controller func(ctx *gin.Context) (*handler.Payload, *exceptions.Exception)
+
+func makeGinFunc(fn controller) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		resp, err := fn(ctx)
+		if err != nil {
+			ctx.JSON(err.Code, err)
+		}
+
+		ctx.JSON(resp.Code, resp)
+	}
 }
--- server/server.go
+++ server/server.go
@@ -2,14 +2,31 @@ package server
 
 import (
 	"github.com/bricsport/handler"
+	"github.com/bricsport/repository"
+	"github.com/bricsport/router"
+	"github.com/bricsport/service"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
-type APIServer struct {
+type Server struct {
 	Router  *gin.Engine
 	Handler handler.Handler
 }
 
-func (as *APIServer) Init() {
-	
+func NewServer(r *gin.Engine, tx *gorm.DB) *Server {
+	repositories := repository.InitRepositories(tx)
+	services := service.InitServices(*repositories)
+	handlers := handler.InitHandlers(*services)
+
+	return &Server{
+		Router:  r,
+		Handler: *handlers,
+	}
+
+}
+
+func (srvr *Server) Init() {
+	// Load routes
+	router.LoadUserRoutes(srvr.Router, srvr.Handler.Userhandler)
 }
